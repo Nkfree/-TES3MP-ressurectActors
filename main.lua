aa1199f5b8dc1ab15cc78f3af715592bd3564266
Nkfree <BlackVaclav@gmail.com>
2020-11-09 09:06:15 +0100

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,267 @@
+local resurrectActorRefIdsA = {}
+resurrectActorRefIdsA["alit"] = true
+resurrectActorRefIdsA["alit_blighted"] = true
+resurrectActorRefIdsA["alit_diseased"] = true
+resurrectActorRefIdsA["ancestor_ghost"] = true
+resurrectActorRefIdsA["ascended_sleeper"] = true
+resurrectActorRefIdsA["ash_ghoul"] = true
+resurrectActorRefIdsA["ash_slave"] = true
+resurrectActorRefIdsA["ash_zombie"] = true
+resurrectActorRefIdsA["ash_zombie_fgaz"] = true
+resurrectActorRefIdsA["atronach_flame"] = true
+resurrectActorRefIdsA["atronach_flame_az"] = true
+resurrectActorRefIdsA["atronach_flame_ttmk"] = true
+resurrectActorRefIdsA["atronach_frost"] = true
+resurrectActorRefIdsA["atronach_frost_ttmk"] = true
+resurrectActorRefIdsA["atronach_storm"] = true
+resurrectActorRefIdsA["atronach_storm_az"] = true
+resurrectActorRefIdsA["atronach_storm_ttmk"] = true
+resurrectActorRefIdsA["balyn omavel"] = true
+resurrectActorRefIdsA["bonelord"] = true
+resurrectActorRefIdsA["bonewalker"] = true
+resurrectActorRefIdsA["bonewalker_greater"] = true
+resurrectActorRefIdsA["bonewalker_weak"] = true
+resurrectActorRefIdsA["centurion_shock_baladas"] = true
+resurrectActorRefIdsA["centurion_sphere"] = true
+resurrectActorRefIdsA["centurion_sphere_hts2"] = true
+resurrectActorRefIdsA["centurion_sphere_nchur"] = true
+resurrectActorRefIdsA["centurion_spider_nchur"] = true
+resurrectActorRefIdsA["centurion_steam"] = true
+resurrectActorRefIdsA["centurion_steam_exhibit"] = true
+resurrectActorRefIdsA["centurion_steam_hts"] = true
+resurrectActorRefIdsA["centurion_steam_nchur"] = true
+resurrectActorRefIdsA["clannfear"] = true
+resurrectActorRefIdsA["cliff racer"] = true
+resurrectActorRefIdsA["cliff racer_blighted"] = true
+resurrectActorRefIdsA["cliff racer_diseased"] = true
+resurrectActorRefIdsA["corprus_lame"] = true
+resurrectActorRefIdsA["corprus_stalker"] = true
+resurrectActorRefIdsA["daedroth"] = true
+resurrectActorRefIdsA["daedroth_az"] = true
+resurrectActorRefIdsA["daedroth_baladas"] = true
+resurrectActorRefIdsA["dreugh"] = true
+resurrectActorRefIdsA["dwarven ghost"] = true
+resurrectActorRefIdsA["guar"] = true
+resurrectActorRefIdsA["guar_feral"] = true
+resurrectActorRefIdsA["hunger"] = true
+resurrectActorRefIdsA["hunger_az_01"] = true
+resurrectActorRefIdsA["hunger_az_02"] = true
+resurrectActorRefIdsA["kagouti"] = true
+resurrectActorRefIdsA["kagouti_blighted"] = true
+resurrectActorRefIdsA["kagouti_diseased"] = true
+resurrectActorRefIdsA["kwama forager blighted"] = true
+resurrectActorRefIdsA["kwama forager"] = true
+resurrectActorRefIdsA["kwama forager_tb"] = true
+resurrectActorRefIdsA["kwama queen_abaesen"] = true
+resurrectActorRefIdsA["kwama queen_ahanibi"] = true
+resurrectActorRefIdsA["kwama queen_akimaes"] = true
+resurrectActorRefIdsA["kwama queen_eluba"] = true
+resurrectActorRefIdsA["kwama queen_eretammus"] = true
+resurrectActorRefIdsA["kwama queen_gnisis"] = true
+resurrectActorRefIdsA["kwama queen_hairat"] = true
+resurrectActorRefIdsA["kwama queen_hhem"] = true
+resurrectActorRefIdsA["kwama queen_madas"] = true
+resurrectActorRefIdsA["kwama queen_maesa"] = true
+resurrectActorRefIdsA["kwama queen_matus"] = true
+resurrectActorRefIdsA["kwama queen_panabanit"] = true
+resurrectActorRefIdsA["kwama queen_sarimisun"] = true
+resurrectActorRefIdsA["kwama queen_shurdan"] = true
+resurrectActorRefIdsA["kwama queen_sinamusa"] = true
+resurrectActorRefIdsA["kwama queen_zalkin"] = true
+resurrectActorRefIdsA["kwama warrior blighted"] = true
+resurrectActorRefIdsA["kwama warrior shurdan"] = true
+resurrectActorRefIdsA["kwama warrior"] = true
+resurrectActorRefIdsA["kwama worker blighted"] = true
+resurrectActorRefIdsA["kwama worker diseased"] = true
+resurrectActorRefIdsA["kwama worker entrance"] = true
+resurrectActorRefIdsA["kwama worker"] = true
+resurrectActorRefIdsA["mudcrab"] = true
+resurrectActorRefIdsA["mudcrab-diseased"] = true
+resurrectActorRefIdsA["mudcrab_hrmudcrabnest"] = true
+resurrectActorRefIdsA["netch_betty"] = true
+resurrectActorRefIdsA["netch_betty_ranched"] = true
+resurrectActorRefIdsA["netch_bull"] = true
+resurrectActorRefIdsA["netch_bull_ranched"] = true
+resurrectActorRefIdsA["nix-hound blighted"] = true
+resurrectActorRefIdsA["nix-hound"] = true
+resurrectActorRefIdsA["nix-hound_dampworm"] = true
+resurrectActorRefIdsA["ogrim titan"] = true
+resurrectActorRefIdsA["ogrim"] = true
+resurrectActorRefIdsA["ogrim_az"] = true
+resurrectActorRefIdsA["rat"] = true
+resurrectActorRefIdsA["rat_blighted"] = true
+resurrectActorRefIdsA["rat_diseased"] = true
+resurrectActorRefIdsA["scamp"] = true
+resurrectActorRefIdsA["scrib blighted"] = true
+resurrectActorRefIdsA["scrib diseased"] = true
+resurrectActorRefIdsA["scrib"] = true
+resurrectActorRefIdsA["shalk"] = true
+resurrectActorRefIdsA["shalk_blighted"] = true
+resurrectActorRefIdsA["shalk_diseased"] = true
+resurrectActorRefIdsA["skeleton archer"] = true
+resurrectActorRefIdsA["skeleton champion"] = true
+resurrectActorRefIdsA["skeleton warrior"] = true
+resurrectActorRefIdsA["skeleton"] = true
+resurrectActorRefIdsA["skeleton_weak"] = true
+resurrectActorRefIdsA["slaughterfish"] = true
+resurrectActorRefIdsA["slaughterfish_small"] = true
+resurrectActorRefIdsA["winged twilight"] = true
+
+local resurrectActorRefIdsB = {}
+resurrectActorRefIdsB["dremora"] = true
+resurrectActorRefIdsB["dremora_lord"] = true
+resurrectActorRefIdsB["golden saint"] = true
+
+local resurrectActorRefIdsC = {}
+resurrectActorRefIdsC["scamp_creeper"] = true
+
+local resurrectTimeA = 60
+local resurrectTimeB = 90
+local resurrectTimeC = 1
+
+local actorsToResurrect = {}
+
+local Methods = {}
+
+Methods.LoadActorsToResurrect = function()
+  local actorsData = jsonInterface.load("custom/actorsToResurrect.json")
+
+  if actorsData == nil then
+    Methods.SaveActorsToResurrect()
+  else
+    actorsToResurrect = actorsData
+  end
+end
+
+Methods.SaveActorsToResurrect = function()
+  jsonInterface.quicksave("custom/actorsToResurrect.json", actorsToResurrect)
+end
+
+Methods.MarkActorToResurrect = function(cellDescription, location, refId, uniqueIndex)
+  actorsToResurrect[uniqueIndex] = {cellDescription = cellDescription, location = location, refId = refId}
+end
+
+
+Methods.GetResurrectTime = function(refId)
+
+  if resurrectActorRefIdsA[refId] then
+    return resurrectTimeA
+  elseif resurrectActorRefIdsB[refId] then
+    return resurrectTimeB
+  elseif resurrectActorRefIdsC[refId] then
+    return resurrectTimeC
+  end
+
+  return nil
+end
+
+Methods.ForceSaveActors = function(cellDescription)
+
+  local cell = LoadedCells[cellDescription]
+
+  if cell then
+    cell:SaveActorPositions()
+  end
+end
+
+Methods.OnActorDeathHandler = function(eventStatus, pid, cellDescription)
+
+  Methods.ForceSaveActors(cellDescription)
+
+  local cell = LoadedCells[cellDescription]
+  local unloadAtEnd = false
+
+  if cell == nil then
+    unloadAtEnd = true
+    logicHandler.LoadCell(cellDescription)
+    cell = LoadedCells[cellDescription]
+  end
+
+  for _, uniqueIndex in pairs(cell.data.packets.death) do
+
+    if actorsToResurrect[uniqueIndex] == nil then -- check if the timer for this uniqueIndex hasn't already started
+      local location = cell.data.objectData[uniqueIndex].location
+      local refId = cell.data.objectData[uniqueIndex].refId
+      local resurrectTime = Methods.GetResurrectTime(refId)
+
+      if resurrectTime ~= nil then
+        Methods.MarkActorToResurrect(cellDescription, location, refId, uniqueIndex)
+        Methods.SaveActorsToResurrect()
+        tes3mp.StartTimer(tes3mp.CreateTimerEx("resurrect_actor", time.seconds(resurrectTime), "s", uniqueIndex))
+      end
+    end
+  end
+
+  if unloadAtEnd then
+    logicHandler.UnloadCell(cellDescription)
+  end
+end
+
+
+function resurrect_actor(uniqueIndex)
+
+  if logicHandler.GetConnectedPlayerCount() < 1 then
+    return
+  end
+
+  local actor = actorsToResurrect[uniqueIndex]
+
+  if actor == nil then
+    return
+  end
+
+  local cell = LoadedCells[actor.cellDescription]
+  local unloadAtEnd = false
+
+  if cell == nil then
+    unloadAtEnd = true
+    logicHandler.LoadCell(actor.cellDescription)
+    cell = LoadedCells[actor.cellDescription]
+  end
+
+  local location = actor.location
+
+  logicHandler.DeleteObjectForEveryone(actor.cellDescription, uniqueIndex)
+  tableHelper.removeValue(cell.data.packets.death, uniqueIndex)
+  tableHelper.removeValue(cell.data.packets.actorList, uniqueIndex)
+  tableHelper.removeValue(cell.data.packets.death, uniqueIndex)
+  tableHelper.removeValue(cell.data.packets.container, uniqueIndex)
+  tableHelper.removeValue(cell.data.packets.equipment, uniqueIndex)
+  tableHelper.removeValue(cell.data.packets.position, uniqueIndex)
+  tableHelper.removeValue(cell.data.packets.spawn, uniqueIndex)
+  tableHelper.removeValue(cell.data.packets.statsDynamic, uniqueIndex)
+  cell.data.objectData[uniqueIndex] = nil
+
+  local newUniqueIndex = logicHandler.CreateObjectAtLocation(actor.cellDescription, location, actor.refId, "spawn")
+
+  tes3mp.LogMessage(enumerations.log.INFO, "[ResurrectActors] " .. actor.refId .. " with new uniqueIndex: " .. newUniqueIndex .. "(" .. uniqueIndex .. ") at " .. actor.cellDescription)
+
+  for _, pl in pairs(Players) do
+    local playerCell = tes3mp.GetCell(pl.pid)
+    if playerCell == actor.cellDescription then
+      cell:LoadObjectsSpawned(pl.pid, cell.data.objectData, cell.data.packets.spawn)
+    end
+  end
+
+  actorsToResurrect[uniqueIndex] = nil
+  Methods.SaveActorsToResurrect()
+
+  if unloadAtEnd then
+    cell:QuicksaveToDrive()
+    logicHandler.UnloadCell(actor.cellDescription)
+  end
+end
+
+customEventHooks.registerHandler("OnServerPostInit", function(eventStatus)
+  Methods.LoadActorsToResurrect()
+end)
+
+customEventHooks.registerHandler("OnPlayerAuthentified", function (eventStatus, pid)
+  for uniqueIndex, data in pairs(actorsToResurrect) do
+    local actor = data
+    local resurrectTime = Methods.GetResurrectTime(actor.refId)
+    tes3mp.StartTimer(tes3mp.CreateTimerEx("resurrect_actor", time.seconds(resurrectTime), "s", uniqueIndex))
+  end
+end)
+
+customEventHooks.registerHandler("OnActorDeath", Methods.OnActorDeathHandler)
